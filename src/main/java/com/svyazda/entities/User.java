408e5024fb1f11857cea7842666081fd51c21f73
jtalgatulu@yandex.kz <jtalgatulu@yandex.kz>
2022-01-31 15:57:53 +0600

    Подробно описал каждую сущность в entities/ и конкретизировал некоторые хранимые ими значения

--- src/main/java/com/svyazda/entities/User.java
+++ src/main/java/com/svyazda/entities/User.java
@@ -1,10 +1,14 @@
 package com.svyazda.entities;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +26,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date createdAt;
 
     private String email;
     private String username;
